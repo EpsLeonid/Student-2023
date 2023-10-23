d622303406fd25c6d3cacc21ebc593c6344d1750
Ikkch-Matthew <matuha570@gmail.com>
2023-10-23 16:53:38 +0700

    23.10

--- Quartus II/Grebnev/D_trigger.sv
+++ Quartus II/Grebnev/D_trigger.sv
@@ -1,9 +1,10 @@
-module D_trigger (d,clk,out);
-input d;
-input clk;
-output out;
+module D_trigger (
+input d,
+input clk,
+output out
+);
 always @(posedge clk)
-begin
-	out <= d;
-end
+	begin
+		out <= d;
+	end
 endmodule
