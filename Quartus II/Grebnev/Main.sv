48b24dd91acafee31622b8b349a0b7141f0e7c42
Ikkch-Matthew <matuha570@gmail.com>
2023-11-14 14:15:30 +0700

    14.11

--- Quartus II/Grebnev/Main.sv
+++ Quartus II/Grebnev/Main.sv
@@ -1,14 +1,18 @@
-module Main(A,B,C,d,clk,out,DATA_OUT);
+import package_settings::*;
+module Main(A,B,C,D,E,F,d,clk,out,DATA_OUT);
 
-input wire [7:0] A;
-input wire [7:0] B;
-input d;
-input clk;
-output wire [7:0] C;
-output out;
-output reg [16:0] DATA_OUT;
+	input wire [SIZE_A-1:0] A,
+	input wire [SIZE_B-1:0] B,
+	input wire [SIZE_C-1:0] C,
+    input wire [7:0] D;
+    input wire [7:0] E;
+    input d;
+    input clk;
+    output wire [15:0] F;
+    output out;
+	output reg [SIZE_DATA_OUT-1:0] DATA_OUT;
 
-signals(A,B,C);
+signals(D,E,F);
 D_trigger(d,clk,out);
 reg8(A,B,C,clk,DATA_OUT);
 
